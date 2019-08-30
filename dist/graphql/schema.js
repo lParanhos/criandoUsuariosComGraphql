74a5981d2d3dc4a2487f41dd6bcf8a4d219ab8fd
lParanhos <leandro-paranhos@hotmail.com>
2019-08-30 10:48:56 -0300

    Primeira api com graphql

---
+++ dist/graphql/schema.js
@@ -0,0 +1,48 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const graphql_tools_1 = require("graphql-tools");
+const users = [
+    {
+        id: 1,
+        name: 'Jon',
+        email: 'jon@email.com'
+    },
+    {
+        id: 2,
+        name: 'Dany',
+        email: 'dany@email.com'
+    }
+];
+const typeDefs = `
+    type User {
+        id: ID!,
+        name: String!,
+        email: String!
+    }
+
+    type Query {
+        allUsers: [User!]!
+    }
+
+    type Mutation{
+        createUser(name: String!, email: String!): User
+    }
+`;
+const resolvers = {
+    /*  User: {
+         id: (user) => user.id,
+         name: (user) => 'Teste Resolver trivial',
+         email: (user) => user.email
+     }, */
+    Query: {
+        allUsers: () => users
+    },
+    Mutation: {
+        createUser: (parent, args) => {
+            const newUser = Object.assign({ id: users.length + 1 }, args);
+            users.push(newUser);
+            return newUser;
+        }
+    }
+};
+exports.default = graphql_tools_1.makeExecutableSchema({ typeDefs, resolvers });
